d7da2f1b05f1b0b24b7cb5301114f596bf3b6094
achinok <37181010+achinok@users.noreply.github.com>
2018-06-06 08:32:16 -0400

    Add files via upload

--- public/cartService.js
+++ public/cartService.js
@@ -1,20 +1,15 @@
+"use strict"
+{
 // Create a CartService in Angular. Give it a getAllItems() method that uses $http to
 // make a GET request to your /cart-items API.
-
-// angular service here 
-
-// const http = require("http");
-// let k = require("cartConnect.js");
-// console.log(k.ks(0));
-
-// http.createServer(onRequest).listen(3005);
-
-// function onRequest(request, response){
-//     response.write(k.ks(0));
-//     response.end();
-// }
-
-app.get("/cart", (req, res) => {
-    res.send("Hello Shoppers!");
-    });
-
+function cartService() {
+    this.getAllItems = () => {
+        app.get("/cart", (req, res) => {
+            res.send("Hello Shoppers!");
+            });  
+        }
+    }   
+    angular
+    .module("app")
+    .service("cartService",cartService);
+    }
